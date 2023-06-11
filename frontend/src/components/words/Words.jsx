a9e80df2bfd50a98c18c0090b69133bcbce50c26
jirohan <hondazmr6@gmail.com>
2023-06-11 09:51:05 +0545

    words added

--- frontend/src/components/words/Words.jsx
+++ frontend/src/components/words/Words.jsx
@@ -1,4 +1,3 @@
 export const Words = [
-    "mula", "chor", "psycho", "daka", "bitch", "nigga", "shit", "butt"
-
+    "mula", "chor", "psycho", "daka", "bitch", "nigga", "shit", "butt", "fuck", "bastard", "asshole", "bullshit", "cunt", "twat", "chutiya", "masi", "naku", "behanchod", "madharchod", "bhosadike", "ghand", "randi", "pussy", "motherfucker" 
 ]
